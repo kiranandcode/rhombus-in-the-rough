fb509880c1b7822ecbbf530a7e2544e877a0e506
Kiran Gopinathan <kirang@pop-os.localdomain>
2023-05-14 10:18:12 +0200

    major refactoring of internals

---
+++ maps/terrain.tsx
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../resources/images/terrain.png" width="1024" height="1024"/>
+ <wangsets>
+  <wangset name="Base" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="2" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="33" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="64" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="65" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="66" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="96" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="97" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="98" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="128" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="129" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="130" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Dark" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="4" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="36" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="37" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="67" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="68" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="69" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="99" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="100" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="101" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="131" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="132" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="133" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="163" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="164" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="165" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+  <wangset name="Grass" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="193" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="194" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="225" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="226" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="256" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="257" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="258" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="288" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="289" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="290" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="320" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="321" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="322" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="352" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="353" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="354" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
